a806d7ce0b03b4d8621ad07af01ca31f4e0a1263
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2026-03-23 22:35:50 -0300

    remove logs de ConfigMap e API_KEY do AppService

--- project/src/app.service.ts
+++ project/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('ConfigMap: ', process.env.APP);
+    console.log('API_KEY: ', process.env.API_KEY);
     return 'Hello World!';
   }
   getHelloK8s(): string {
